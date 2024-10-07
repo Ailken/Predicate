15ef4df87432a5314875a8e74ffda62f7307e30b
ailken <ailkencf@gmail.com>
2024-10-07 17:17:17 -0300

    Reference method com método não estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -30,6 +30,10 @@ public class Product {
         return p.getPrice() >= 100.00;
     }
 
+    public boolean nonstaticProductPredicate() {
+        return price >= 100.00;
+    }
+
     @Override
     public String toString() {
         return name + "," + String.format("%.2f", price);
